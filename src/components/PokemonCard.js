2a9db23cc90c8b897841b76ae989c56c5e80d837
Corina Udrescu <corinutz@gmail.com>
2022-09-21 09:06:31 +0200

    Prettier reformat

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const PokemonCard = ({ pokemon, liked, onLike }) => (
   <div
     style={{
-      border: '1px solid lightgrey',
-      margin: '10px',
-      padding: '0 10px 10px 10px',
-      width: '120px',
+      border: "1px solid lightgrey",
+      margin: "10px",
+      padding: "0 10px 10px 10px",
+      width: "120px",
     }}
   >
-    <h3 style={{ lineHeight: '1.8rem' }}>
+    <h3 style={{ lineHeight: "1.8rem" }}>
       {pokemon.name}
-      {liked && ' ❤️'}
+      {liked && " ❤️"}
     </h3>
     {!liked && <button onClick={() => onLike(pokemon.name)}>Like</button>}
   </div>
