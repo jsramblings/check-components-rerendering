7e34bd09a7c1664e8df2f77068ead65738a2c67c
Corina Udrescu <corinutz@gmail.com>
2022-11-28 15:23:37 +0100

    Prevent jumps

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -5,16 +5,16 @@ const PokemonCard = ({ pokemon, liked, onLike }) => (
     style={{
       border: "1px solid lightgrey",
       margin: "10px",
-      padding: "0 10px 10px 10px",
-      width: "120px",
+      padding: "0 10px 20px 10px",
+      width: "140px",
+      textAlign: "center",
     }}
   >
-    <h3 style={{ lineHeight: "1.8rem" }}>
-      {pokemon.name}
-      {liked && " ❤️"}
-    </h3>
-    {!liked && <button onClick={() => onLike(pokemon.name)}>Like</button>}
+    <h3 style={{ lineHeight: "1.8rem" }}>{pokemon.name}</h3>
+    {liked ? " ❤️" : <button onClick={() => onLike(pokemon.name)}>Like</button>}
   </div>
 );
+
+PokemonCard.whyDidYouRender = true;
 
 export default PokemonCard;
